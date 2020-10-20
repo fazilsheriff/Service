673789cdc27a130e69e504f4b8258591560177b8
fazil.s@lantrasoft.com <SinCos123>
2020-10-20 11:47:57 +0530

    simple start and stop service

---
+++ app/src/main/java/com/example/service/SampleService.kt
@@ -0,0 +1,31 @@
+package com.example.service
+
+import android.app.Service
+import android.content.Intent
+import android.os.IBinder
+import android.util.Log
+
+class SampleService : Service() {
+    private  val TAG = "SampleService"
+    override fun onBind(intent: Intent): IBinder {
+        TODO("Return the communication channel to the service.")
+    }  
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.i(TAG, "In Service class: Displays the current thread id "+Thread.currentThread().name.toString())
+
+//        for(i in 1..13)
+//        {
+//            Thread.sleep(1000)
+//            showLog("Service is doing some thing!!"+i.toString())
+//        }
+        return super.onStartCommand(intent, flags, startId)
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy: Serivce is Destoryed")
+    }
+    fun showLog(MSG:String)
+    {
+        Log.i(TAG, MSG)
+    }
+}
