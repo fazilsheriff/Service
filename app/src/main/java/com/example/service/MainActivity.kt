b175f8d6886fca63cfcd9c120d96df100df8a10f
fazil.s@lantrasoft.com <SinCos123>
2020-12-02 12:15:46 +0530

    From UI tried thread to stop. I can  stat thread but unable to stop

--- app/src/main/java/com/example/service/MainActivity.kt
+++ app/src/main/java/com/example/service/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.service
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    private  val TAG = "Threading"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnStartService = findViewById<Button>(R.id.btnstart)
+        val btnStopService = findViewById<Button>(R.id.stopservice)
+
+        btnStartService.setOnClickListener(){
+            Log.i(TAG, "Inside the on 1: ")
+            for(i in 1..10)
+            {
+                if(Thread.interrupted())
+                {
+                    Log.i(TAG, "Inside thread interuted!!")
+                }
+                showLog("Service is doing some thing!!"+i.toString())
+                Thread.sleep(5000)
+            }
+        }
+
+        btnStopService.setOnClickListener(){
+            Log.i(TAG, "Inside the on 2: ")
+            Thread.interrupted()
+
+        }
+
+
+
+    }
+
+    private fun showLog(toString: String) {
+        Log.i(TAG, "$toString")
+
     }
 }
