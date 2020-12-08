2d910462b6436466ece42f033a8c9210836214b9
fazil.s@lantrasoft.com <SinCos123>
2020-12-08 18:49:46 +0530

    What if service is not found in the manifiest

--- app/src/main/java/com/example/service/MainActivity.kt
+++ app/src/main/java/com/example/service/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.example.service
+///Todo:1 How to check service is running or not
+///Todo:2 Life cycle of UnBounded Service
+///Todo:3 Differencec between Stop servvice and stopSelf()
+///Todo:4 What is exported and enabled
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnStart.setOnClickListener{
+            val intent= Intent(this,LifeCycleService::class.java)
+            startService(intent)
+
+        }
+
+
+        btnStop.setOnClickListener{
+            val intent= Intent(this,LifeCycleService::class.java)
+            stopService(intent)
+        }
+
     }
 }
