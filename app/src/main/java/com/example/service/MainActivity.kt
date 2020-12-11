93a6cbd9628a4486c733ec823c88da46402da050
fazil.s@lantrasoft.com <SinCos123>
2020-12-11 13:56:34 +0530

    1 Calling two services parallel and tried to finish the application. 2 Even it tried to close the finish the app it runs in background

--- app/src/main/java/com/example/service/MainActivity.kt
+++ app/src/main/java/com/example/service/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.service
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val intent= Intent(this,FirstService::class.java)
+        val intent2= Intent(this,SecondService::class.java)
+
+        btnStart.setOnClickListener{
+            startService(intent)
+            startService(intent2)
+            Thread.sleep(4000)
+            finish()
+
+        }
+
+        btnStop.setOnClickListener{
+            stopService(intent)
+        }
     }
 }
